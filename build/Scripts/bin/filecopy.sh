3fb3ce828316072b026f33630ee4ebcf5c2d2d5b
dhinesh <dhineshelango@gmail.com>
2021-12-30 15:17:50 +0530

    updated

--- build/Scripts/bin/filecopy.sh
+++ build/Scripts/bin/filecopy.sh
@@ -35,8 +35,7 @@ if curl ${url} -o ${conf_dir}/file.xml
     then
     _log "[INFO]: File downloaded Successfully and saved in ${conf_dir}"
     _log "[INFO]: Pushing the file to /data"
-    mv 
-else ${conf_dir}/file.xml /data/
+else 
     _log "[ERROR]: Error in Downloading the file"
     exit 1
 fi
